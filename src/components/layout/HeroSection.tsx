bcb6a75b7583d7c833dffbda56c6949148095d42
blop666 <leongame986@gmail.com>
2025-05-10 14:32:05 +0700

    Add AOS library for animations and implement custom animation styles

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -1,9 +1,21 @@
 import HeroSectionTitle from "../ui/hero/title"
 import VideoPlaceholder from "../ui/hero/videoPlaceholder"
 import WhatsAppButton from "../ui/hero/whatsappButton"
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default function HeroSection() {
+  useEffect(() => {
+        AOS.init({
+          duration: 1000, // Global animation duration
+          once: true, // Only animate once
+        });
+      }, []);
+
+
   return(
-    <div className="flex flex-col items-center justify-center bg-[#121212]">
+    <div className="flex flex-col items-center justify-center bg-[#121212]" data-aos="fade-up" data-aos-duration="1000">
       <HeroSectionTitle />
       <VideoPlaceholder />
       <WhatsAppButton />
--- src/components/ui/hero/videoPlaceholder.tsx
+++ src/components/ui/hero/videoPlaceholder.tsx
@@ -1,7 +1,21 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './aos-custom.css'
 
 export default function VideoPlaceholder()   {
+  useEffect(() => {
+          AOS.init({
+            duration: 1000, // Global animation duration
+            once: true, // Only animate once
+          });
+        }, []);
+
   return (
-    <div className="w-[80%] h-[40svw] bg-[#292929] rounded-3xl mt-10 flex justify-center items-center">
+    <div className="w-[80%] h-[40svw] bg-[#292929] rounded-3xl mt-10 flex justify-center items-center" 
+    data-aos="flip-down-lite"
+    data-aos-delay="100"
+    data-aos-duration="1000">
       <button className="w-[8%] h-[6.33svw] rounded-full bg-[#a8a8a8] active:bg-[#8a8a8a] flex justify-center items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
